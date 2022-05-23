fbfc9a8f9dd9427619ceebef2e742e153ebc77f2
AbdullahBoz <abdullahboz@outlook.com>
2022-05-23 20:39:09 +0300

    Zero/Negative/Positive
    girilen tam sayının negatif pozitif ve sıfır olma durumu

---
+++ practice3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice3
+{
+    internal class Program
+    {
+        // Girilen tamsayının sıfır, pozitif ya da negatip olup olmadığını bulan midi code.
+        static void Main(string[] args)
+        {
+            int number;
+           
+            while (true)
+                // While true ise sonsuz döngü olur .
+            {
+                Console.WriteLine("Enter the random number: ");
+                number = Convert.ToInt32(Console.ReadLine());
+                if (number == 0)
+                    Console.WriteLine("Number is Zero!");
+                else if (number < 0)
+                    Console.WriteLine("Number is Negative!");
+                else if (number > 0)
+                    Console.WriteLine("Number is Positive!");
+           
+            }
+            
+        }
+    }
+}
