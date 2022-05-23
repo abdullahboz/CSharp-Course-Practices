bddd5eded501a6a2d12213eb1b44e95827663e89
AbdullahBoz <abdullahboz@outlook.com>
2022-05-23 20:39:34 +0300

    Tax
    Kdv hesaplama

---
+++ Practice4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice4
+{
+    internal class Program
+    {
+        // Girilen Fiyata KDV(%18) ekleyerek yazdırma mini kod.
+        static void Main(string[] args)
+        {
+            double price, total;
+            Console.WriteLine("Enter price to find price with tax");
+            price = Convert.ToDouble(Console.ReadLine());
+            total = price * 1.18;
+            Console.WriteLine("{0} Price with tax: ", total);
+            Console.ReadKey();
+
+        }
+    }
+}
